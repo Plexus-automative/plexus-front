e795d05d79097f497379e2fb0f2e3c96028a16a6
koussaymoujahed7-jpg <koussaymoujahed7@gmail.com>
2025-11-21 21:35:54 +0100

    change collapse to item

--- src/menu-items/applications.ts
+++ src/menu-items/applications.ts
@@ -58,7 +58,7 @@ const applications: NavItemType = {
     {
       id: "commandes-emis",
       title: "commandes-emis",
-      type: "collapse",
+      type: "item",
       breadcrumbs: false,
       icon: icons.invoice,
       url: "/pages/commandes-emis",
