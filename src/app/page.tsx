5e8c93c98fd26f8da1a36d742f071e27d630a22d
ihebmoujahed <mjhdiheb02@gmail.com>
2026-02-25 09:18:37 +0100

    fix /login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,7 @@
-// material-ui
-import Divider from '@mui/material/Divider';
+import { redirect } from 'next/navigation';
 
-// project-imports
-import Hero from 'sections/landing/Hero';
-import Technologies from 'sections/landing/Technologies';
-import Combo from 'sections/landing/Combo';
-import FigmaBlock from 'sections/landing/FigmaBlock';
-import Apps from 'sections/landing/Apps';
-import Free from 'sections/landing/Free';
-import Testimonial from 'sections/landing/Testimonial';
-import Partner from 'sections/landing/Partner';
-import Subscribe from 'sections/landing/Subscribe';
-import SimpleLayout from 'layout/SimpleLayout';
+// ==============================|| ROOT PAGE ||============================== //
 
-// ==============================|| LANDING PAGE ||============================== //
-
-export default function Landing() {
-  return (
-    <SimpleLayout>
-      <Hero />
-      <Technologies />
-      <Combo />
-      <FigmaBlock />
-      <Apps />
-      <Free />
-      <Testimonial />
-      <Partner />
-      <Subscribe />
-      <Divider sx={{ borderColor: 'secondary.light' }} />
-    </SimpleLayout>
-  );
+export default function Page() {
+  redirect('/login');
 }
